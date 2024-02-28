93d3a82996fd7276f66bd229358b9d18c323d50f
faisal04 <glimpsebd@gmail.com>
2024-02-29 00:53:26 +0600

    first commit

---
+++ forLoop.py
@@ -0,0 +1,26 @@
+# printing each employee in theemployee list
+employee = ["Arif", "Tushar", "Tabit", "Awal", "Habib"]
+for i in employee:
+    print(i)
+
+# loop through string
+
+for x in "Banana":
+    print(x)
+# loop using a break
+
+fruit = ["Apple", "Banana", "Cherry", "Dove"]
+for y in fruit:
+    print(y)
+    if y == "Banana":
+        break
+# exit loop when fruit is banana
+
+fruit = ["Apple", "Banana", "Cherry", "Dove"]
+for z in fruit:
+    if z == "Banana":
+        break
+    print(z)
+
+
+
